24b0cfbde73f9473d224141c8bd51d142ebe38f9
Peter Titus <titusmpeter@gmail.com>
2025-05-05 17:50:31 +0100

    preferences

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,9 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+const x = '';
+
+const calcAge = yob => 2050 - yob;
+console.log(calcAge(1990));
+console.warn(calcAge(1990));
+console.error(calcAge(1990));
